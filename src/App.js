df801779be6efbacdec84cfa90f4db82831fc1d4
Josh Pollock <josh@calderawp.com>
2018-07-30 20:48:37 -0400

    rough in post editor to app

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {Posts} from './components/Posts/Posts';
+import {PostEdit} from "./components/PostEdit/PostEdit";
 
 class App extends Component {
 
 	constructor(props) {
 		super(props);
 		this.state = {
-			posts: []
+			posts: [],
+			postToEdit: 0,
 		};
+		this.getEditPost = this.getEditPost.bind(this);
+		this.updatePostToEdit = this.updatePostToEdit.bind(this);
 	}
 
+	getEditPost(){
+		return this.state.posts.find( post => this.state.postToEdit === post.ID);
+	}
+
+	updatePostToEdit(postToEdit){
+		this.setState({postToEdit})
+	}
 	render() {
 		return (
 			<div className="App">
@@ -26,13 +37,26 @@ class App extends Component {
 				<p className="App-intro">
 					To get started, edit <code>src/App.js</code> and save to reload.
 				</p>
-				<div>
+
 					{this.state.posts.length &&
-					<Posts
-						posts={this.state.posts}
-					/>
+						<div>
+							<Posts
+								posts={this.state.posts}
+							/>
+
+							{0 != this.state.postToEdit &&
+								<PostEdit
+									className={'main-editor'}
+									post={this.getEditPost()}
+									onChange={() => {
+									}}
+								/>
+							}
+
+						</div>
+
 					}
-				</div>
+
 			</div>
 		);
 	}
--- src/App.test.js
+++ src/App.test.js
@@ -4,12 +4,13 @@ import App from './App';
 import {mount} from 'enzyme';
 import Enzyme from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
+
 Enzyme.configure({adapter: new Adapter()});
 
 it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+	const div = document.createElement('div');
+	ReactDOM.render(<App/>, div);
+	ReactDOM.unmountComponentAtNode(div);
 });
 
 /** Mock WordPress post to test with**/
@@ -34,12 +35,49 @@ const posts = [
 	},
 ];
 
-describe( 'Rendering posts in app', () => {
-  it( 'Shows the posts', () => {
-	  const component = mount(
-		  <App/>
-	  );
-	  component.setState({posts: posts});
-	  expect(component.find('.post-list-wrapper').length).toEqual(2);
-  });
+describe('Rendering posts in app', () => {
+	it('Shows the posts', () => {
+		const component = mount(
+			<App/>
+		);
+		component.setState({posts: posts});
+		expect(component.find('.post-list-wrapper').length).toEqual(2);
+	});
+
+
+	it('Finds the post to edit ', () => {
+		const component = mount(
+			<App/>
+		);
+		component.setState({posts: posts});
+		component.setState({postToEdit: '2'});
+		expect(component.instance().getEditPost().ID).toEqual('2');
+	});
+
+	it('Shows the editor', () => {
+		const component = mount(
+			<App/>
+		);
+		component.setState({posts: posts});
+		component.setState({postToEdit: '2'});
+		expect(component.find('.main-editor').length).toBe(2);
+	});
+
+	it('Does not show the editor', () => {
+		const component = mount(
+			<App/>
+		);
+		component.setState({posts: posts});
+		expect(component.find('.main-editor').length).toBe(0);
+	});
+
+	it('updates the edit post', () => {
+		const component = mount(
+			<App/>
+		);
+		component.instance().updatePostToEdit(5);
+		expect(component.state('postToEdit')).toBe(5);
+	});
+
+
 });
