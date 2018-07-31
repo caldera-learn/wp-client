d16e4670c5c59a98b784804d2e3fd2543875f67f
Josh Pollock <josh@calderawp.com>
2018-07-30 20:01:32 -0400

    test for posts list

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import {mount} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter()});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+
+/** Mock WordPress post to test with**/
+const posts = [
+	{
+		ID: '1',
+		title: {
+			rendered: 'Hi Roy'
+		},
+		content: {
+			rendered: 'Lorem ipsum, etc.'
+		}
+	},
+	{
+		ID: '2',
+		title: {
+			rendered: 'Hi Mike'
+		},
+		content: {
+			rendered: 'Lorem ipsum, etc.'
+		}
+	},
+];
+
+describe( 'Rendering posts in app', () => {
+  it( 'Shows the posts', () => {
+	  const component = mount(
+		  <App/>
+	  );
+	  component.setState({posts: posts});
+	  expect(component.find('.post-list-wrapper').length).toEqual(2);
+  });
 });
--- src/components/Posts/Posts.test.js
+++ src/components/Posts/Posts.test.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {Posts} from "./Posts";
 import renderer from 'react-test-renderer';
+import {mount} from 'enzyme';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+Enzyme.configure({adapter: new Adapter()});
+
 
 describe('Posts component', () => {
 	/** Mock WordPress post to test with**/
@@ -45,4 +50,15 @@ describe('Posts component', () => {
 		expect(component.toJSON()).toMatchSnapshot();
 	});
 
+	describe( 'Posts in loop', () => {
+		it('Renders the right number of children', () => {
+			const component = mount(
+				<Posts
+					posts={posts}
+					className={'post-list-wrapper'}
+				/>
+			);
+			expect(component.find('.post-list-wrapper').length).toEqual(2);
+		});
+	});
 });
